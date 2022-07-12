a4fae1555a7fea50ebe3897417fee39ae4a240c9
Lamzey Aribido <48647671+Lamzey@users.noreply.github.com>
2022-07-12 11:26:48 +0100

    First Commit

--- web_request.py
+++ web_request.py
@@ -1,5 +1,7 @@
 import requests
+import socket
 
+print("Enter your website in this manner ==> 'https://www.facilitar.org' to know if it's working or not.")
 response = requests.get(input())
 
 if response.status_code == 200:
@@ -8,3 +10,9 @@ elif response.status_code == 400:
     print('Oops, Page Not Found!')
 else:
     print('We Are Working On The Website')
+
+print("Enter your website in this manner ==> 'www.facilitar.org' to get the IP Address.")
+web = input()
+ip_add = socket.gethostbyname(web)
+
+print("The IP Address For " + web + " is " + ip_add)
